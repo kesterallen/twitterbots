f766f1b197c0de8bc4834e6ae82a6d06cdd4502c
Kester Allen <kallen@amyris.com>
2017-03-21 11:28:53 -0700

    BUG: Fixed error in box width, simplfied code a bit

--- twitter_magellanvenusbot.py
+++ twitter_magellanvenusbot.py
@@ -1,5 +1,4 @@
 
-import datetime
 import math
 import os
 from PIL import Image, ImageOps
@@ -24,29 +23,35 @@ IMG_TMPL = "".join([
         'HEIGHT=%s',
 ])
 
-def get_bounding_box(zoom=1.0):
-    # Math from http://mathworld.wolfram.com/SpherePointPicking.html
-    rand_u = random.random()
-    rand_v = random.random()
-    lng = 360.0 * rand_u # Range: [0.0, 360.0]
-    lat = math.acos(2.0 * rand_v - 1) * 180.0 / math.pi - 90.0 # Range: [-90.0, 90.0]
+def get_bounding_box(lat_box_side, aspect_ratio):
+    is_bad_box = True
+    while is_bad_box:
+        # Math from http://mathworld.wolfram.com/SpherePointPicking.html
+        rand_u = random.random()
+        rand_v = random.random()
+        lng = 360.0 * rand_u # Range: [0.0, 360.0]
+        lat = math.acos(2.0 * rand_v - 1) * 180.0 / math.pi - 90.0 # Range: [-90.0, 90.0]
 
-    # Convert latitude to radans for the cos operation
-    lat_rad = lat * math.pi / 180.0
+        # Convert latitude to radans for the cos operation
+        lat_rad = lat * math.pi / 180.0
+        lng_box_side = lat_box_side * math.cos(abs(lat_rad)) * aspect_ratio
 
-    d_lng = 1.5 * zoom # degrees
-    d_lat = d_lng / math.cos(abs(lat_rad))
+        lng_stop = lng + lng_box_side
+        lat_stop = lat + lat_box_side
 
-    return lng, lat, lng + d_lng, lat + d_lat
+        is_bad_box = lng_stop >= 360.0 or abs(lat_stop) >= 90.0
 
-def get_random_venus_image(width, height, zoom=1.0):
+    return lng, lat, lng_stop, lat_stop
+
+def get_random_venus_image(width, height, lat_box_side):
     # Get a subimage without too many black pixels:
     #
     # TODO: this should probably be a stringio
+    aspect_ratio = float(width) / float(height)
     image_fn = "tmpvenus.jpg"
     image_too_dark = True
     while image_too_dark:
-        lng, lat, lng_end, lat_end = get_bounding_box(zoom)
+        lng, lat, lng_end, lat_end = get_bounding_box(lat_box_side, aspect_ratio)
         str_box = "%s,%s,%s,%s" % (lng, lat, lng_end, lat_end)
         url = IMG_TMPL % (str_box, width, height)
         urllib.urlretrieve(url, image_fn)
@@ -60,14 +65,17 @@ def get_random_venus_image(width, height, zoom=1.0):
 def main():
     width = 1920
     height = 1080
-    zoom = random.uniform(1.0, 3.0)
-    lng, lat, url, image_fn = get_random_venus_image(width, height, zoom)
+    lat_box_side = random.uniform(1.0, 5.0) # degrees
 
-    keys = get_keys(__file__)
+    lng, lat, url, image_fn = get_random_venus_image(width, height, lat_box_side)
+
     twitter = BotTweet()
     twitter.words = [
-        "Venus, latitude: %.5f longitude: %.5f, %s" % (lat, lng, url)
+        "Venus, latitude: %.2f " % lat,
+        "longitude: %.2f, " % lng,
+        "%.1f degrees latitude width. %s" % (lat_box_side, url)
     ]
+    keys = get_keys(__file__)
     twitter.publish_with_image(image_fn, *keys)
 
     os.remove(image_fn)
