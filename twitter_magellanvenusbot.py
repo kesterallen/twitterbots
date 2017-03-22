8c58336d4bc6863a6c9b3fb313fb9b1d8db5f1bf
Kester Allen <kallen@amyris.com>
2017-03-21 19:39:23 -0700

    ENH: clean up of bounding box handling

--- twitter_magellanvenusbot.py
+++ twitter_magellanvenusbot.py
@@ -1,4 +1,5 @@
 
+import attrdict
 import math
 import os
 from PIL import Image, ImageOps
@@ -36,12 +37,20 @@ def get_bounding_box(lat_box_side, aspect_ratio):
         lat_rad = lat * math.pi / 180.0
         lng_box_side = lat_box_side * math.cos(abs(lat_rad)) * aspect_ratio
 
-        lng_stop = lng + lng_box_side
-        lat_stop = lat + lat_box_side
+        lng_end = lng + lng_box_side
+        lat_end = lat + lat_box_side
 
-        is_bad_box = lng_stop >= 360.0 or abs(lat_stop) >= 90.0
+        box_width_km = 105.6 * lat_box_side
 
-    return lng, lat, lng_stop, lat_stop
+        is_bad_box = lng_end >= 360.0 or abs(lat_end) >= 90.0
+
+    return attrdict.AttrDict({
+        'lng': lng,
+        'lat': lat,
+        'lng_end': lng_end,
+        'lat_end': lat_end,
+        'box_width_km': box_width_km
+    })
 
 def get_random_venus_image(width, height, lat_box_side):
     # Get a subimage without too many black pixels:
@@ -51,8 +60,8 @@ def get_random_venus_image(width, height, lat_box_side):
     image_fn = "tmpvenus.jpg"
     image_too_dark = True
     while image_too_dark:
-        lng, lat, lng_end, lat_end = get_bounding_box(lat_box_side, aspect_ratio)
-        str_box = "%s,%s,%s,%s" % (lng, lat, lng_end, lat_end)
+        box = get_bounding_box(lat_box_side, aspect_ratio)
+        str_box = "%s,%s,%s,%s" % (box.lng, box.lat, box.lng_end, box.lat_end)
         url = IMG_TMPL % (str_box, width, height)
         urllib.urlretrieve(url, image_fn)
         image = Image.open(image_fn)
@@ -60,21 +69,21 @@ def get_random_venus_image(width, height, lat_box_side):
 
     image = ImageOps.autocontrast(image)
     image.save(image_fn)
-    return lng, lat, url, image_fn
+
+    return box, url, image_fn
 
 def main():
     width = 1920
     height = 1080
-    lat_box_side = random.uniform(1.0, 5.0) # degrees
-    box_width_km = 105.6 * lat_box_side # km
+    lat_box_side = random.uniform(0.5, 5.0) # degrees
 
-    lng, lat, url, image_fn = get_random_venus_image(width, height, lat_box_side)
+    box, url, image_fn = get_random_venus_image(width, height, lat_box_side)
 
     twitter = BotTweet()
     twitter.words = [
-        "Venus, latitude: %.2f " % lat,
-        "longitude: %.2f, " % lng,
-        "%.1f km wide. %s" % (box_width_km, url)
+        "Venus, latitude: %.1f " % box.lat,
+        "longitude: %.1f, " % box.lng,
+        "%.1f km wide. %s" % (box.box_width_km, url)
     ]
     twitter.publish_with_image(image_fn)
 
