cfdb7a5fcbacc90d8c1791205ae2bb5dfb317b1b
Kester Allen <kallen@amyris.com>
2021-12-07 21:13:37 -0800

    LNT: pylint suggestions

--- tweetbot_lib/__init__.py
+++ tweetbot_lib/__init__.py
@@ -22,6 +22,7 @@ class BotTweet:
         self.words = [] if word is None else [word[:MAX_TWEET_LEN]]
 
     def pop(self):
+        """Pop a word off of self.words"""
         return self.words.pop()
 
     def append(self, word):
@@ -34,29 +35,36 @@ class BotTweet:
         return self.len > MAX_TWEET_LEN
 
     def can_take_new_word(self, word):
-        """Will tweet be too long with new word"""
+        """Will tweet not be too long with new word"""
         return not self.will_be_too_long(word)
 
     def will_be_too_long(self, word):
-        assert(isinstance(word, str))
+        """Will tweet be too long with new word"""
+        assert isinstance(word, str)
         will_be_length = self.len + len(word)
         return will_be_length > MAX_TWEET_LEN
 
     @property
     def len(self):
+        """Length of tweet text"""
         return len(self.str)
 
     @property
     def str(self):
+        """String representation of text"""
         return " ".join(self.words)
 
     @property
-    def twitter_keys(self, keyfile="../keys.txt"):
+    def twitter_keys(self):
         """Assume the keys file is ../keys.txt"""
+        return self.twitter_keys_fn()
+
+    def twitter_keys_fn(self, keyfile="../keys.txt"):
+        """Keys"""
         auth_keys = {}
 
         keyfile_full = os.path.join(os.path.dirname(__file__), keyfile)
-        with open(keyfile_full) as keys_fh:
+        with open(keyfile_full, encoding='utf-8') as keys_fh:
             for row in keys_fh.readlines():
                 name, value = row.strip().split('=')
                 auth_keys[name] = value
@@ -67,12 +75,14 @@ class BotTweet:
         return keys
 
     def publish(self, debug=False):
+        """Publish a tweet"""
         twitter = Twython(*self.twitter_keys)
         if debug:
             print(self.str)
         return twitter.update_status(status=self.str)
 
     def publish_with_image(self, image_fn, debug=False):
+        """Publish a tweet with an image"""
         twitter = Twython(*self.twitter_keys)
         with open(image_fn, 'rb') as image:
             response = twitter.upload_media(media=image)
@@ -81,8 +91,12 @@ class BotTweet:
             ids = [response['media_id']]
             return twitter.update_status(status=self.str, media_ids=ids)
 
-    def download_tweet_text(self, tweet_generate_url):
-        resp = requests.get(tweet_generate_url)
+    def download_tweet_text(self, tweet_api_url):
+        """
+        Get a tweet's text from an API, which should return a JSON object
+        with a 'tweet' key
+        """
+        resp = requests.get(tweet_api_url)
         text = resp.json()['tweet']
         self.words = [text]
 
@@ -100,17 +114,17 @@ def tweetify_text(textfile, use_lines=False):
 
     If use_lines is True, make one tweet per line in the file.
     """
-    def _tweets_by_line(text_fh):
+    def _tweets_by_line(fileh):
         """
         One file line per tweet, truncated to MAX_TWEET_LEN if necessary.
         Returns a list of BotTweet objects.
         """
-        tweets = [BotTweet(l) for l in text_fh.readlines()]
+        tweets = [BotTweet(l) for l in fileh.readlines()]
         return tweets
 
-    def _tweets_by_word(text_fh):
+    def _tweets_by_word(fileh):
         """
-        Make tweets by appending words from text_fh, staying below MAX_TWEET_LEN
+        Make tweets by appending words from fileh, staying below MAX_TWEET_LEN
         characters in length.
         Returns a list of BotTweet objects.
         """
@@ -121,7 +135,7 @@ def tweetify_text(textfile, use_lines=False):
         #
         tweets = []
         tweet = BotTweet()
-        for word in text_fh.read().split():
+        for word in fileh.read().split():
             if tweet.can_take_new_word(word):
                 tweet.append(word)
             else:
@@ -133,22 +147,31 @@ def tweetify_text(textfile, use_lines=False):
         tweets.append(tweet)
         return tweets
 
-    with open(textfile) as fh:
-        tweets = _tweets_by_line(fh) if use_lines else _tweets_by_word(fh)
+    with open(textfile, encoding='utf-8') as fileh:
+        tweets = _tweets_by_line(fileh) if use_lines else _tweets_by_word(fileh)
     return tweets
 
 def get_today_index(num_tweets, then):
+    """
+    Get today's index into a list that is num_tweets long, starting at 'then'
+    """
     now = datetime.datetime.now()
     td_since_start = now - then
     today_index = td_since_start.days % num_tweets
     return today_index
 
 def get_today_tweet(tweets, then):
+    """
+    Get today's tweet text for the list 'tweets' starting at 'then'
+    """
     today_index = get_today_index(len(tweets), then)
     today_tweet = tweets[today_index]
     return today_tweet
 
 def parse_text_and_get_today_tweet(textfile, start_date, use_lines=False):
+    """
+    Get today's tweet text from a file, starting at 'then'
+    """
     tweetfile = get_tweet_filename(textfile)
     tweets = tweetify_text(tweetfile, use_lines)
     return get_today_tweet(tweets, start_date)
