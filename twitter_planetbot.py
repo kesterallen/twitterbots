91359e1ce45c8afc4dec0a9d3c9c87fd327c88e7
Kester Allen <kallen@amyris.com>
2021-12-08 12:42:59 -0800

    LNT: black reformat

--- twitter_planetbot.py
+++ twitter_planetbot.py
@@ -15,25 +15,32 @@ from tweetbot_lib import BotTweet
 
 DEBUG = False
 
-PLANETS = AttrDict({
-    'Venus': AttrDict({
-        'map': '/maps/venus/venus_simp_cyl.map',
-        'layers': 'MAGELLAN',
-        'botname': 'venusbot',
-        'lat_box_side_degrees': (0.1, 3.0),
-        'km_per_lat_deg': 105.6,
-    }),
-    'Mercury': AttrDict({
-        'map': '/maps/mercury/mercury_simp_cyl.map',
-        'layers':'MESSENGER_Color',
-        'botname': 'mercurybot',
-        'lat_box_side_degrees': (1.0, 20.0),
-        'km_per_lat_deg': 42.58,
-    })
-})
+PLANETS = AttrDict(
+    {
+        "Venus": AttrDict(
+            {
+                "map": "/maps/venus/venus_simp_cyl.map",
+                "layers": "MAGELLAN",
+                "botname": "venusbot",
+                "lat_box_side_degrees": (0.1, 3.0),
+                "km_per_lat_deg": 105.6,
+            }
+        ),
+        "Mercury": AttrDict(
+            {
+                "map": "/maps/mercury/mercury_simp_cyl.map",
+                "layers": "MESSENGER_Color",
+                "botname": "mercurybot",
+                "lat_box_side_degrees": (1.0, 20.0),
+                "km_per_lat_deg": 42.58,
+            }
+        ),
+    }
+)
+
 
 def usgs_url(planet_data, box, width, height):
-    """ Thanks to Trent Hare of the USGS for this service """
+    """Thanks to Trent Hare of the USGS for this service"""
     return (
         "https://planetarymaps.usgs.gov/cgi-bin/mapserv?"
         "SERVICE=WMS&VERSION=1.1.1&SRS=EPSG:4326&STYLES=&REQUEST=GetMap&"
@@ -42,8 +49,10 @@ def usgs_url(planet_data, box, width, height):
         f"WIDTH={width}&HEIGHT={height}&map={planet_data.map}"
     )
 
+
 class BoundingBox:
     """A bounding box in latitude/longitude with the correct aspect ratio"""
+
     def __init__(self, lng_lat_side, aspect_ratio, km_per_lat_deg):
         """Create a new box"""
         self.lng = lng_lat_side.lng
@@ -77,8 +86,10 @@ class BoundingBox:
     @property
     def pretty_str(self):
         """More verbose string representation"""
-        return(f"latitude: {self.lat:.1f} longitude: {self.lng:.1f}, "
-            f"{self.box_height_km:.1f} km wide")
+        return (
+            f"latitude: {self.lat:.1f} longitude: {self.lng:.1f}, "
+            f"{self.box_height_km:.1f} km wide"
+        )
 
     @property
     def lat_gmap(self):
@@ -102,18 +113,20 @@ def get_bounding_box(lat_box_side, aspect_ratio, km_per_lat_deg):
         # lat range: [-90.0, 90.0]
         lng = 360.0 * rand_u
         lat = acos(2.0 * rand_v - 1) * 180.0 / pi - 90.0
-        lng_lat_side = AttrDict({'lng': lng, 'lat': lat, 'side': lat_box_side})
+        lng_lat_side = AttrDict({"lng": lng, "lat": lat, "side": lat_box_side})
 
         box = BoundingBox(lng_lat_side, aspect_ratio, km_per_lat_deg)
         is_bad_box = box.is_bad
 
     return box
 
+
 def _gauss(x, *p):
     """Define model function to be used to fit to the data"""
     # pylint: disable=invalid-name
     A, mu, sigma = p
-    return A * np.exp(-(x-mu)**2/(2.0*sigma**2))
+    return A * np.exp(-((x - mu) ** 2) / (2.0 * sigma ** 2))
+
 
 def _ignore(hist, offset=10, width=3.0):
     """
@@ -121,12 +134,12 @@ def _ignore(hist, offset=10, width=3.0):
     to hist[offset:] (ignore first offset bins). Default ignore range is
     everything outside +/- 3 std from the center of the gaussian fit.
     """
-    #pylint: disable=unused-variable,unbalanced-tuple-unpacking
+    # pylint: disable=unused-variable,unbalanced-tuple-unpacking
 
     # p0 is the initial guess for the fitting coefficients (A, mu, & sigma)
-    p_max = max(hist[offset:]) # value of biggest peak in hist, excluding 0:offset
-    p_ctr = hist.index(p_max) # the index location of that value
-    p_std = 20.0 # initial guess, ~10% of hist width
+    p_max = max(hist[offset:])  # value of biggest peak in hist, excluding 0:offset
+    p_ctr = hist.index(p_max)  # the index location of that value
+    p_std = 20.0  # initial guess, ~10% of hist width
     params_initial = [p_max, p_ctr, p_std]
 
     # Perform the fit, and get the fit coefficients:
@@ -134,8 +147,8 @@ def _ignore(hist, offset=10, width=3.0):
     coeff, var_matrix = optimize.curve_fit(_gauss, xdata, hist, params_initial)
 
     # Get the left and right edges of +/- 3 std dev from the fit center:
-    data_start = int(coeff[1] - width*coeff[2])
-    data_end = int(coeff[1] + width*coeff[2])
+    data_start = int(coeff[1] - width * coeff[2])
+    data_end = int(coeff[1] + width * coeff[2])
 
     # If the fit is out of bounds, revert to very conservative values:
     if data_start < 0 or data_start > 255:
@@ -152,16 +165,18 @@ def _ignore(hist, offset=10, width=3.0):
 
     return ignore
 
+
 def _get_image(lat_box_side, width, height, planet_data):
     """Get a subimage to check for black pixel amount"""
     aspect_ratio = float(width) / float(height)
     box = get_bounding_box(lat_box_side, aspect_ratio, planet_data.km_per_lat_deg)
     url = usgs_url(planet_data, box, width, height)
-    tmp_fn, headers = urllib.request.urlretrieve(url) #pylint: disable=unused-variable
-    image = Image.open(tmp_fn).convert('L')
-    return(box, url, tmp_fn, image)
+    tmp_fn, headers = urllib.request.urlretrieve(url)  # pylint: disable=unused-variable
+    image = Image.open(tmp_fn).convert("L")
+    return (box, url, tmp_fn, image)
 
-def get_random_planet_image(planet_data, width, height, lat_box_side, max_pct_black=0.5):
+
+def random_planet_image(lat_box_side, width, height, planet_data, max_pct_black=0.5):
     """Get a subimage without too many black pixels"""
 
     image_too_dark = True
@@ -183,29 +198,32 @@ def get_random_planet_image(planet_data, width, height, lat_box_side, max_pct_bl
 
     return box, url, image_fn
 
+
 def main():
     """
     Generate and publish a planet image
     """
     planet_name = sys.argv[1]
     assert planet_name in PLANETS
+    planet_data = PLANETS[planet_name]
 
     width = 1920
     height = 1080
     lat_box_side = random.uniform(*PLANETS[planet_name].lat_box_side_degrees)
 
-    box, url, image_fn = get_random_planet_image(PLANETS[planet_name], width, height, lat_box_side)
+    box, url, image_fn = random_planet_image(lat_box_side, width, height, planet_data)
 
     if DEBUG:
         print(box.pretty_str, url)
 
     tweet_text = f"{planet_name}, {box.pretty_str}, {url}"
-    twitter = BotTweet(word=tweet_text, botname=PLANETS[planet_name]['botname'])
+    twitter = BotTweet(word=tweet_text, botname=PLANETS[planet_name]["botname"])
 
     if not DEBUG:
         twitter.publish_with_image(image_fn)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
 
 # For Venus:
