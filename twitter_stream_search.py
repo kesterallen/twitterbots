2e30f04c702be5ced64fde4d41212b9b1a7df52a
Kester Allen <kallen@amyris.com>
2021-12-08 12:23:41 -0800

    LNT: Black reformat

--- twitter_stream_search.py
+++ twitter_stream_search.py
@@ -9,30 +9,38 @@ import requests
 from twython import TwythonStreamer
 from tweetbot_lib import BotTweet
 
+
 def now():
     """Nicely formatted "now" timestamp"""
-    return datetime.datetime.now().strftime('%Y-%m-%d %H:%M')
+    return datetime.datetime.now().strftime("%Y-%m-%d %H:%M")
+
 
 class TweetStreamer(TwythonStreamer):
     """Implement on_* methods for parent class TwythonStreamer"""
-    def on_success(self, data): #pylint: disable=no-self-use
+
+    def on_success(self, data):  # pylint: disable=no-self-use
         """Success method"""
-        if 'text' in data:
-            name = data['user']['screen_name']
+        if "text" in data:
+            name = data["user"]["screen_name"]
             url = f"https://twitter.com/{name}/status/{data['id']}"
-            text = data['text']
+            text = data["text"]
             print(f"\n{now()} {name} {url}\n{text}")
 
-    def on_error(self, status_code, data): #pylint: disable=no-self-use
+    def on_error(
+        self, status_code, data
+    ):  # pylint: disable=no-self-use,arguments-differ
         """error handling"""
         print(f"{now()}: in on_error")
         print(status_code, data)
         time.sleep(10)
         self.disconnect()
 
-    def on_timeout(self, status_code=None, data=None): #pylint: disable=no-self-use
+    def on_timeout(
+        self, status_code=None, data=None
+    ):  # pylint: disable=no-self-use,arguments-differ
         """timeout handling"""
         print(f"{now()}: timeout. {status_code} / {data}")
+
 
 def main():
     """Scan twitter stream for argument string"""
@@ -52,8 +60,9 @@ def main():
             requests.exceptions.ChunkedEncodingError,
             socket.error,
         ) as err:
-            # print(now(), "restarting ", type(err).__name__, err)
+            print(now(), "restarting ", type(err).__name__, err)
             time.sleep(60)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
