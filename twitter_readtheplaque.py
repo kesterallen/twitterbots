97ea4be5339142710c7cb6e255dafe0fd444ef80
Kester Allen <kallen@amyris.com>
2022-08-05 21:22:01 -0700

    ENH: Streamline automatic tweeting of featured plaque

--- twitter_readtheplaque.py
+++ twitter_readtheplaque.py
@@ -1,27 +1,35 @@
 """ Set the featured plaque for the day and tweet about it """
 import requests
-
 from tweetbot_lib import BotTweet
 
-TWEET_URL = "https://readtheplaque.com/tweet"
-FEATURED_GEOJSON_URL = "https://readtheplaque.com/featured/geojson"
+URL_PREF = "https://readtheplaque.com/"
+URLS = {
+    "RAND": URL_PREF + "featured/random",
+    "TWEET": URL_PREF + "tweet",
+    "GEOJSON": URL_PREF + "featured/geojson",
+}
+
 
 def main():
     """
-    Set the featured plaque for the day and tweet it, also send a tweet to the
-    submitter if they are specified in the plaque description
+    1) Set the featured plaque for the day,
+    2) Publish a tweet about it,
+    3) send a tweet to the plaque's submitter (if they are specified in the plaque description)
     """
-    twitter = BotTweet()
-    twitter.download_tweet_text(TWEET_URL)
+    resp = requests.get(URLS["RAND"])
+    resp_json = resp.json()
+
+    twitter = BotTweet(resp_json["tweet"])
     twitter.publish()
 
-    # Send a tweet to the submitter, if specified
-    resp = requests.get(FEATURED_GEOJSON_URL)
-    submitter_tweet_text = resp.json()["submitter_tweet"]
-    if submitter_tweet_text is not None:
-        twitter.words = [submitter_tweet_text]
+    # Send a tweet to the submitter (if the plaque's geojson specifies a
+    # submitter, see Models.py:Plaque:tweet_to_plaque_submitter in
+    # https://github.com/kesterallen/read-the-plaque)
+    #
+    if submitter_tweet_text := resp_json["submitter_tweet"]:
+        twitter = BotTweet(submitter_tweet_text)
         twitter.publish()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
